ad350d638d71cd0f86be85c3df5c2f84bba13450
Adam Horky <adam.horky@ul.com>
2018-04-19 07:34:37 -0500

    Webpack works!

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import './global.css';
+
+const style = {
+    color: '#00f'
+};
+
+const Index = () => {
+    return <div style={style} className="greeting">Hello React!</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById("index"));
