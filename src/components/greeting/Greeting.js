500254cc8f73e160392ccbd7d5fe8ac9ef3ff596
horkman <adam.horky06@gmail.com>
2018-04-22 08:51:24 -0500

    Adding Prettier support

--- src/components/greeting/Greeting.js
+++ src/components/greeting/Greeting.js
@@ -1,25 +1,22 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 class Greeting extends React.Component {
+  style = {
+    color: "#00f"
+  };
 
-    style = {
-        color: '#00f'
-    };
-    
-    render() {
-        return (
-            <div
-                style={this.style}
-                className="greeting">
-                <h1>Hello {this.props.name}!</h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div style={this.style} className="greeting">
+        <h1>Hello {this.props.name}!</h1>
+      </div>
+    );
+  }
 }
 
 Greeting.propTypes = {
-    name: PropTypes.string
-  };
+  name: PropTypes.string
+};
 
 export default Greeting;
